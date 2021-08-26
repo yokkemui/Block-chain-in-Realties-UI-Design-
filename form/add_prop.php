47a9a121b0ccf140529084d3cdd787a9f43eed05
yokkemui <76992280+yokkemui@users.noreply.github.com>
2021-08-26 12:05:50 +0800

    Add files via upload

---
+++ form/add_prop.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+ 
+  Welcome <?php echo $_GET["lname"]; ?><br>
+  Your email address is: <?php echo $_GET["Fee2"]; ?>
+
+</body>
+
+</html>
---
+++ form/form.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<?php
+
+$name = $_GET[“name”];
+
+$age = $_GET[“Pin”];
+
+echo “Your name is “. $name . ” and you are “. $Pin . ” old”.
+
+?>
+
+</body>
+</html>
